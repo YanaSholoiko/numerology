11a2537083da8fdbeea063c9e7bc08bae2cf12f0
YanaSholoiko <yanasholouko@gmail.com>
2024-12-08 12:02:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-git add main.py
-git commit -m "Add main.py"
-git push origin main
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    import os
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
