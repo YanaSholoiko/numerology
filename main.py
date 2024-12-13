2d2ddda85b07d108c56ac9c70fec4e0773ceea36
YanaSholoiko <yanasholouko@gmail.com>
2024-12-13 15:32:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
+
+@app.route('/debug-files')
+def debug_files():
+    files = []
+    for root, dirs, filenames in os.walk('.'):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return '<br>'.join(files)
+
+
+
+
 from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
